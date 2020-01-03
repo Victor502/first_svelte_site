6aa5bda6a4a2ac3cbb32955ec13080d8c9a4b3c5
Victor502 <victorahorton@gmail.com>
2020-01-02 21:24:48 -0500

    first commit

---
+++ src/components/Signin.svelte
@@ -0,0 +1,29 @@
+<script>
+    let count = 0;
+    $: doubled = count * 2
+    function handleClick() {
+	count += 1;
+}
+</script>
+<style>
+form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-self: center;
+    margin: 0 auto;
+    max-width: 250px
+}
+button {
+    display: flex;
+    justify-content: center;
+    margin: 0 auto;
+}
+</style>
+
+<form>
+    Name: <input type="text" />
+    Password: <input type="text" />
+</form>
+<button on:click={handleClick}> Sign In</button>
+{count} <p>and doubled it</p> {doubled}
---
+++ src/routes/contact.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+	<title>Contact</title>
+</svelte:head>
+
+<script>
+    import Signin from "../components/Signin.svelte";
+</script>
+
+<Signin />
